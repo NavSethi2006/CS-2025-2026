0e13d6884fbfaa9415b0b93dbe9d8816e9fbeefc
NavSethi2006 <navinsethi2006@gmail.com>
2025-09-27 21:09:47 -0400

    FINISHED CP264 assignment

---
+++ cp264/assignments/a3/mystring.c
@@ -0,0 +1,67 @@
+/*
+ * your program signature
+ */ 
+ 
+ #include "mystring.h"
+
+
+int str_words(char *s) {
+ // your code
+
+    if (s == 0) return -1;
+    int count = 0;
+    int in_word = 0;
+    char *p = s;
+    while (*p) {
+        if ((*p >= 'A' && *p <= 'Z' ) || (*p >= 'a' && *p <= 'z' )) {
+            if(!in_word) {
+                in_word = 1;
+            }
+        }else if(*p == ' ' || *p == '\t' || *p == ',' || *p == '.') {
+            if(in_word) {
+                count++;
+                in_word = 0;
+            }
+        }
+        p++; // move to next
+    }
+    if(in_word) {
+        count++;
+    }
+
+    return count;
+}
+ 
+
+int str_lower(char *s) {
+    if (s == 0) return -1;
+    int shift_val = 32;
+    int count = 0;
+    char *p = s;
+    while(*p) {
+        if((*p >= 'A' && *p <= 'Z' )) {
+            *p += shift_val;
+            count++;
+        }
+        p++;
+    }
+ }
+ 
+
+void str_trim(char *s) {
+    char *p=s, *dp=s;
+    int last_was_space = 0;
+    while(*p) {
+        if(*p != ' ' || (p > s && *(p-1) != ' ')) {
+            *dp = *p;
+            dp++;
+        }
+        p++;
+    }
+    if(*(p-1)!=' ') {
+        *dp='\0';
+    }else {
+        *(dp-1) = '\0';
+    }
+
+}
---
+++ cp264/assignments/a3/myword.c
@@ -0,0 +1,81 @@
+
+#include <stdio.h>
+#include <string.h>
+#include "mystring.h"
+#include "myword.h"
+
+#define MAX_LINE_LEN 1000
+#define MAX_WORDS 1000
+
+
+
+int create_dictionary(FILE *fp, char *dictionary) {
+// your code
+
+    char line[1000];
+    char delimiters[] = " .,\n\t\r";
+    char *token;
+    int words_added = 0;
+    while (fgets(line, MAX_WORDS, fp) != NULL) {
+        str_lower(line);
+        str_trim(line);
+        token = (char*) strtok(line, delimiters);
+        while (token != NULL) {
+            strcat(dictionary, token);
+            strcat(dictionary, ",");
+            token = (char*) strtok(NULL, delimiters);
+            words_added++;   
+        } 
+
+    }
+
+    return words_added;
+}
+
+
+BOOLEAN contain_word(char *dictionary, char *word) {
+    if(strstr(dictionary, word) != NULL) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+
+WORDSTATS process_words(FILE *fp, WORD *words, char *dictionary) {
+
+    char line[1000];
+    char delimiters[] = " .,\n\t\r";
+    WORDSTATS ws = {0,0,0};
+    char *word_token;
+    int word_index = 0;
+
+    while(fgets(line, MAX_LINE_LEN, fp) != NULL) {
+        ws.line_count++;
+        str_lower(line);
+        word_token = (char *) strtok(line, delimiters);
+        while (word_token != NULL) {
+            ws.word_count++;
+
+            if (contain_word(dictionary, word_token) == FALSE) {
+                // search in words array
+                int found = 0;
+                for (int i = 0; i < word_index; i++) {
+                    if (strcmp(words[i].word, word_token) == 0) {
+                        words[i].count++;
+                        found = 1;
+                        break;
+                    }
+                }
+                if (!found && word_index < MAX_WORDS) {
+                    strcpy(words[word_index].word, word_token);
+                    words[word_index].count = 1;
+                    word_index++;
+                    ws.keyword_count++;
+                }
+            }
+            word_token = strtok(NULL, delimiters);
+        }
+    }
+    return ws;
+} 
+
