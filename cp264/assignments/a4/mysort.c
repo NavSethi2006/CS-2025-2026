fab32be6ba5c1aa36b24ed38e394f6b11f613a65
= <=>
2025-09-30 10:13:26 -0400

    changes

--- cp264/assignments/a4/mysort.c
+++ cp264/assignments/a4/mysort.c
@@ -28,14 +28,42 @@ int cmp(void *x, void *y) {
 void select_sort(void *a[], int left, int right)
 {
 // your code
+
+     for (int i = 0; i < right; ++i) {
+          int k = i;
+          for(int j = i+1; j < right ; ++j) {
+               if (cmp(a[j], a[k]) <0) {
+                    k = j;
+               }
+          }
+          if (i != k) { // the following swap a[i] and a[k]
+               swap(&a[i], &a[k]);
+          }
+     }
 }
 
 void quick_sort(void *a[], int left, int right)
 { 
 // your code
+     if( left < right ) {
+          int i = left+1;
+          int j = right;
+          while(i <= j) {
+               while( i <= right && cmp( a[i], a[left])<=0) i++;
+               while( j >= left && cmp(a[j] , a[left]) > 0) j--;
+               if( i < j ) {
+                    swap (&a[i], &a[j]); 
+               }
+          }
+          swap(&a[left], &a[j]);
+          quick_sort(a, left, j-1);
+          quick_sort(a, j+1, right);
+     }
+
 }
 
 void my_sort(void *a[], int left, int right, int (*cmp)(void*, void*) )
 { 
-// your code
+
+
 }
